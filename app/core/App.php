17ea6183f67b0c124074f215cdb4494b593335da
hanas_bp <hanasoke@gmail.com>
2019-11-23 22:37:59 +0700

    membuat routing pada mvc

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,23 @@
 class App {
     public function __construct()
     {
-        echo 'Yes I can';
+        // untuk cek apa saja yang akan diisikan user pada url
+        // var_dump($_GET);
+
+        $url = $this->parseURL();
+        var_dump($url);
+
+    }
+
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            // $url = $_GET['url'];
+            // untuk menghapus / maka gunakan rtrim
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
