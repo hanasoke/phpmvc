2618a75a22b183ac2b7bbee3ff12177ea28f530c
hanas_bp <hanasoke@gmail.com>
2019-11-25 17:16:38 +0700

    memasukkan Assets Bootstrap dan Gambar dalam mvc

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,7 @@
-    <h1>About Me</h1>
-    <p>Halo, nama saya <?= $data['name'] ?>, umur saya <?= $data['age']; ?> Tahun, saya adalah seorang <?= $data['job']; ?></p>
-    
+<div class="container mt-4">
+
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/photo.png" alt="Hanas Bayu Pratama" class="rounded-circle shadow">
+    <p>Halo, my name is <?= $data['name'] ?>, my age is <?= $data['age']; ?> Years, I'm <?= $data['job']; ?></p>
+
+</div>
